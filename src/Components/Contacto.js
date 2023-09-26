0cd3f35b78cc0d63eedc5fdccff9ef28485a4efd
ismaponto <isma.pontoriero@gmail.com>
2023-09-26 09:34:50 +0200

    Correcion tipeo y clendly

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -5,7 +5,7 @@ const Contacto = () => {
   return (
     <div className="App">
       <PopupWidget
-        url="https://calendly.com/ismaponto"
+        url="https://calendly.com/mativiettro"
         /*
          * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
          * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -14,12 +14,12 @@ const Jumbotron = () => {
         Con el metodo High Strategie vamos a ordenar tus esfuerzos para alcanzar las metas</p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a href="#Contacto" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-Quiero Saber como ya            <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+Quiero saber más           <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
           </a>
-          <a href="#Metodo" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
-Quizas mas tarde          </a>  
+          {/* <a href="#Metodo" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+Quizas mas tarde          </a>   */}
         </div>
       </div>
     </section>
