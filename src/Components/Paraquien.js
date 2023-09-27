988f5b08809ef42dfebdf848c69ea1c20445bd94
ismaponto <isma.pontoriero@gmail.com>
2023-09-27 16:07:04 +0200

    cambio de acordeon por flexcon graficos y textos alternados

--- src/Components/Paraquien.js
+++ src/Components/Paraquien.js
@@ -1,58 +1,41 @@
-import React from "react";
+import React from 'react';
+import imgpersona from "./Paraquien/people.svg"
+import imgemprendedor from "./Paraquien/emprendedor.svg"
 
-function Paraquien () {return(
-    <div id="accordion-collapse" data-accordion="collapse" className="text-center text-blue-50">
-      <h2 id="accordion-collapse-heading-1">
-        <button type="button" className="flex items-center justify-between text-blue-50 bg-indigo-800 w-full p-5 font-medium text-left  border border-b-0 border-blue-50 rounded-t-xl focus:ring-4  text-blue-50 focus:ring-blue-50 dark:focus:ring-blue-50  dark:border-blue-50 dark:text-blue-50 hover:bg-gray-100 dark:hover:bg-gray-100" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
-          <span>Es el curso correcto para mi?</span>
-          <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-          </svg>
-        </button>   
-      </h2>
-      <div id="accordion-collapse-body-1" className="hidden" aria-labelledby="accordion-collapse-heading-1">
-        <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-        <p className="mb-2 text-gray-500 text-bold dark:text-gray-400">Este servicio esta orientado hacia:</p>
-          <p className="text-gray-500 dark:text-gray-400">Emprendedores digitales que se sienten desorganizados</p>
-          <p className="text-gray-500 dark:text-gray-400"> Emprendedores que no tienen un plan de acción</p>
-          <p className="text-gray-500 dark:text-gray-400">Empresas que sus niveles de facturación no crecen</p>
-          <p className="text-gray-500 dark:text-gray-400">Personas que no saben ordenar sus objetivos y se sienten improductivas</p>
-          <p className="text-gray-500 dark:text-gray-400">Personas que no saben gestionar su tiempo.</p>
-        </div>
+function Paraquien() {
+  return (<div className="flex flex-col w-full justify-center md:flex-col  ">
+  <h4 className="mb-2 flex flex-col center justify-center w-full text-3xl bg-indigo-800 w-100 text-blue-50 text-bold dark:text-white h-48 "> 
+  Es el m'etodo Hig Strategie para mi?
+  </h4>
+  
+    <div className="flex p-5 flex-row flex-wrap md:flex-col w-full justify-center  ">
+    <div className="flex flex-none inline-flex ">
+        <img className="w-96" src={imgpersona} alt="personas" /></div>
+    
+      <div className="flex flex-col flex-0 center text-xl text-gray-500 justify-center p-8">
+      <p>Para personas que se sienten desorganizadas y no tiene un plan.</p>
+      <p>Para personas que no saben ordenar sus objetivos y se sienten improductivas.</p>
       </div>
-      <h2 id="accordion-collapse-heading-2">
-        <button type="button" className="flex items-center justify-between w-full p-5 font-medium text-left text-blue-50 border border-b-0 border-gray-200 bg-indigo-800 focus:ring-4 focus:ring-blue-50 dark:focus:ring-blue-50 dark:border-gray-700 dark:text-blue-50 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
-          <span>Tenes algun bonus?</span>
-          <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-          </svg>
-        </button>
-      </h2>
-      <div id="accordion-collapse-body-2" className="hidden" aria-labelledby="accordion-collapse-heading-2">
-        <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-        <p className="mb-2 text-gray-500 text-bold dark:text-gray-400">3 Ebooks de Emprendimiento</p>
-          <p className="text-gray-500 dark:text-gray-400">Planillas de Organizacion y Planificacion Personal</p>
-          <p className="text-gray-500 dark:text-gray-400"> Grabaciones club "Mente Estrategica"</p>
-          <p className="text-gray-500 dark:text-gray-400">Material de Tecnicas de Fijacion de METAS</p>
-          <p className="text-gray-500 dark:text-gray-400">Ademas, si quieres serguir los programas "Recupera tu foco" y Emprender: Mas alla de los Mitos, tendras un 25% de descuento</p>
-</div>
+     </div>
+
+
+
+    <div className="flex p-5 flex-row flex-wrap w-full justify-center md:flex-col">
+        <div className="flex flex-col center text-xl text-gray-500 justify-center p-8">
+          <p>Para personas que se sienten desorganizadas y no tiene un plan.</p>
+          <p>Para personas que no saben ordenar sus objetivos y se sienten improductivas.</p>
       </div>
-      <h2 id="accordion-collapse-heading-3">
-        <button type="button" className="flex items-center justify-between w-full p-5 font-medium text-left text-blue-50 bg-indigo-800 border border-gray-200 focus:ring-4 focus:ring-blue-50 dark:focus:ring-blue-50 dark:border-gray-700 dark:text-blue-400 hover:bg-gray-100 dark:hover:bg-gray-100" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
-          <span>Y si no funciona?</span>
-          <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-          </svg>
-        </button>
-      </h2>
-      <div id="accordion-collapse-body-3" className="hidden" aria-labelledby="accordion-collapse-heading-3">
-        <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
-        <p className="text-gray-500 dark:text-gray-400">Garantía de Resultados</p>
-        <p className="text-gray-500 dark:text-gray-400">Garantía del 100 % del dinero o trabajamos gratis hasta conseguirlos</p>
-        </div>
+      <div className="flex flex-none inline-flex">
+        <img img className="w-96"src={imgemprendedor} alt="emprendedores" />
       </div>
     </div>
+
+ 
     
-)
+  
+  
+  </div>
+  );
 }
-export default Paraquien
+
+export default Paraquien;
