c5ed91178ed22ca3b91f9e7c9a6f3b7f7d456070
ismaponto <isma.pontoriero@gmail.com>
2023-09-29 16:36:38 +0200

    corregir casos de exito en columna

--- src/Components/CasosdeExito.js
+++ src/Components/CasosdeExito.js
@@ -6,16 +6,16 @@ import CasodeExito3 from './casosDeExito/CasodeExito3';
 
 const CasodeExito = () => {
   return (
-<div id={"exito"} className="flex flex-wrap w-full justify-center md:flex-col  ">
+<div id={"exito"} className="flex flex row flex-wrap w-full justify-center  ">
 <h4 className="mb-2 flex flex-col center justify-center font-manrope w-full text-3xl bg-indigo-800 w-100 text-blue-50 text-bold dark:text-white h-36 ">Algunas personas que ya han seguido el curso</h4>
 
-  <div className="p-5 sm:max-w-sm">
+  <div className="p-5">
     <CasodeExito1  />
   </div>
-  <div className="p-5 sm:max-w-sm">
+  <div className="p-5">
     <CasodeExito2  />
   </div>
-  <div className="p-5 sm:max-w-sm">
+  <div className="p-5">
     <CasodeExito3 />
   </div>
   
