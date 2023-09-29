e90e98e6c3ce7619f622e53568ea18cdaf133c70
ismaponto <isma.pontoriero@gmail.com>
2023-09-29 13:11:54 +0200

    chau flowbite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import 'flowbite';
 
 import React from "react";
 // import ReactDOM from "react-dom";
@@ -7,7 +6,7 @@ import NavigationBar from "./Components/NavigationBar"
 import Jumbotron from './Components/Jumbotron';
 import HighStrategy from "./Components/HighStrategy"
 import CasosdeExito from "./Components/CasosdeExito"
-// import Stats from './Components/Stats';
+import Stats from './Components/Stats';
 import Contacto from "./Components/Contacto"
 import Footer from "./Components/Footer"
 import Paraquien from './Components/Paraquien';
@@ -25,7 +24,7 @@ function App() {
 <Paraquien  />
 <CasosdeExito />
 <SoyMatias />
-{/* <Stats /> */}
+<Stats />
 
 
 <Footer />
