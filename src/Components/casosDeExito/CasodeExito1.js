193ecef6bbfbdc28ec0911d9ebb606104b7ba0ef
ismaponto <isma.pontoriero@gmail.com>
2023-11-16 16:09:34 +0100

    a

--- src/Components/casosDeExito/CasodeExito1.js
+++ src/Components/casosDeExito/CasodeExito1.js
@@ -8,7 +8,7 @@ function CasodeExito1() {
         <div className="p-5">
             <h5 className="mb-2 text-2xl font-bold font-manrope tracking-tight text-gray-900 dark:text-white">Clara Heidi</h5>
          
-          <p className="mb-3 font-normal  font-manrope text-gray-700 dark:text-gray-400">Desde que trabajo con Matias, no solo mis ventas se duplicaron, mis dias son mas faciles!</p>
+          <p className="mb-3 font-normal  font-manrope text-gray-700 dark:text-gray-400">Desde que  con Matias, no solo mis ventas se duplicaron, mis dias son mas faciles!</p>
          
         </div>
       </div>
