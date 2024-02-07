a0ede4232ccf15f2edbefba4a28b4c3fedd45262
ismaponto <isma.pontoriero@gmail.com>
2024-02-07 20:09:52 +0100

    cambios fullpage post

--- src/Components/FullPost.js
+++ src/Components/FullPost.js
@@ -1,7 +1,9 @@
 // FullPost.js
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
+import NavigationBar from '../Components/NavigationBar.js';
+import Contacto from '../Components/Contacto.js';
+import Footer from '../Components/Footer.js';
 const FullPost = ({ posts }) => {
 	const { postId } = useParams();
 	const post = posts.find((p) => p.id === parseInt(postId, 10));
@@ -12,7 +14,10 @@ const FullPost = ({ posts }) => {
 
 	return (
 		<div>
+            <NavigationBar />
 			<h1> {post.title} </h1> <img src={post.thumbnail} alt={post.title} /> <p> {post.content} </p>{' '}
+            <Contacto />
+            <Footer />
 		</div>
 	);
 };
